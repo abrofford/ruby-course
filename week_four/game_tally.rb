f39e4c53434eaed165d833ba402b8e8227834b9e
abrofford <32464827+abrofford@users.noreply.github.com>
2017-11-21 01:35:32 -0500

    Update game_tally.rb
    created a separate method to apply points, wins, losses, etc.

--- week_four/game_tally.rb
+++ week_four/game_tally.rb
@@ -12,96 +12,40 @@ class FootballTally
         game = line.split(";")
         case game[0]
         when "Devastating Donkeys"
-          if game[2].chomp == "win"
-            STATS[:"#{game[0]}"][:points] += 3
-            STATS[:"#{game[0]}"][:wins] += 1
-            STATS[:"#{game[1]}"][:loss] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "loss"
-            STATS[:"#{game[0]}"][:points] += 0
-            STATS[:"#{game[1]}"][:points] += 3
-            STATS[:"#{game[0]}"][:loss] += 1
-            STATS[:"#{game[1]}"][:wins] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "draw"
-            STATS[:"#{game[0]}"][:points] += 1
-            STATS[:"#{game[1]}"][:points] += 1
-            STATS[:"#{game[0]}"][:draw] += 1
-            STATS[:"#{game[1]}"][:draw] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          end
+          calculate_stats(game)
         when "Allegoric Alaskans"
-          if game[2].chomp == "win"
-            STATS[:"#{game[0]}"][:points] += 3
-            STATS[:"#{game[0]}"][:wins] += 1
-            STATS[:"#{game[1]}"][:loss] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "loss"
-            STATS[:"#{game[0]}"][:points] += 0
-            STATS[:"#{game[1]}"][:points] += 3
-            STATS[:"#{game[0]}"][:loss] += 1
-            STATS[:"#{game[1]}"][:wins] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "draw"
-            STATS[:"#{game[0]}"][:points] += 1
-            STATS[:"#{game[1]}"][:points] += 1
-            STATS[:"#{game[0]}"][:draw] += 1
-            STATS[:"#{game[1]}"][:draw] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          end
+          calculate_stats(game)
         when "Blithering Badgers"
-          if game[2].chomp == "win"
-            STATS[:"#{game[0]}"][:points] += 3
-            STATS[:"#{game[0]}"][:wins] += 1
-            STATS[:"#{game[1]}"][:loss] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "loss"
-            STATS[:"#{game[0]}"][:points] += 0
-            STATS[:"#{game[1]}"][:points] += 3
-            STATS[:"#{game[0]}"][:loss] += 1
-            STATS[:"#{game[1]}"][:wins] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "draw"
-            STATS[:"#{game[0]}"][:points] += 1
-            STATS[:"#{game[1]}"][:points] += 1
-            STATS[:"#{game[0]}"][:draw] += 1
-            STATS[:"#{game[1]}"][:draw] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          end
+          calculate_stats(game)
         when "Courageous Californians"
-          if game[2].chomp == "win"
-            STATS[:"#{game[0]}"][:points] += 3
-            STATS[:"#{game[0]}"][:wins] += 1
-            STATS[:"#{game[1]}"][:loss] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "loss"
-            STATS[:"#{game[0]}"][:points] += 0
-            STATS[:"#{game[1]}"][:points] += 3
-            STATS[:"#{game[0]}"][:loss] += 1
-            STATS[:"#{game[1]}"][:wins] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          elsif game[2].chomp == "draw"
-            STATS[:"#{game[0]}"][:points] += 1
-            STATS[:"#{game[1]}"][:points] += 1
-            STATS[:"#{game[0]}"][:draw] += 1
-            STATS[:"#{game[1]}"][:draw] += 1
-            STATS[:"#{game[0]}"][:matches_played] += 1
-            STATS[:"#{game[1]}"][:matches_played] +=1
-          end
+          calculate_stats(game)
         end
       end
       p STATS
+    end
+  end
+
+  def self.calculate_stats(game)
+    if game[2].chomp == "win"
+      STATS[:"#{game[0]}"][:points] += 3
+      STATS[:"#{game[0]}"][:wins] += 1
+      STATS[:"#{game[1]}"][:loss] += 1
+      STATS[:"#{game[0]}"][:matches_played] += 1
+      STATS[:"#{game[1]}"][:matches_played] +=1
+    elsif game[2].chomp == "loss"
+      STATS[:"#{game[0]}"][:points] += 0
+      STATS[:"#{game[1]}"][:points] += 3
+      STATS[:"#{game[0]}"][:loss] += 1
+      STATS[:"#{game[1]}"][:wins] += 1
+      STATS[:"#{game[0]}"][:matches_played] += 1
+      STATS[:"#{game[1]}"][:matches_played] +=1
+    elsif game[2].chomp == "draw"
+      STATS[:"#{game[0]}"][:points] += 1
+      STATS[:"#{game[1]}"][:points] += 1
+      STATS[:"#{game[0]}"][:draw] += 1
+      STATS[:"#{game[1]}"][:draw] += 1
+      STATS[:"#{game[0]}"][:matches_played] += 1
+      STATS[:"#{game[1]}"][:matches_played] +=1
     end
   end
 end
